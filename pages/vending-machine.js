c662e8e4406c4ff3d6ac43e4513025c7df1d26f5
simon <simon.lu@tron.network>
2022-12-13 09:34:25 +0800

    c

--- pages/vending-machine.js
+++ pages/vending-machine.js
@@ -6,13 +6,17 @@ import Web3 from "web3";
 const VendingMachine = () => {
   let web3;
   //window.ethereum
-  const connectWalletHandler = () => {
+  const connectWalletHandler = async () => {
     if (
       typeof window !== "undefined" &&
       typeof window.ethereum !== "undefined"
     ) {
-      window.ethereum.request({ method: "eth_requestAccounts" });
-      web3 = new Web3(window.ethereum);
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        web3 = new Web3(window.ethereum);
+      } catch (err) {
+        console.log(err.message);
+      }
     } else {
       // metamask not installed
       console.log("Please install MetaMask!");
